536ea448ab7cc5e284560f251e1ba25a4663d389
Klaus Thaler <97182272+Klaus-Thaler@users.noreply.github.com>
2024-09-20 10:57:47 +0200

    Add files via upload

---
+++ changeBootanimation/scripts/32-bootanimation.sh
@@ -0,0 +1,59 @@
+#!/sbin/sh
+#
+# ADDOND_VERSION=2
+#
+# Waehrend eines System-Upgrades sichert dieses Skript
+# die hinzugefuegten Systemanwendungen und stellt sie nach
+# der Formatierung von /system wieder her.
+#
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+product/media/bootanimation.zip
+product/media/shutanimation.zip
+EOF
+}
+
+if [ -z $backuptool_ab ]; then
+  SYS=$S
+  TMP=/tmp
+else
+  SYS=/postinstall/system
+  TMP=/postinstall/tmp
+fi
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/"$FILE"
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Set permissions
+    for i in $(list_files); do
+      chown root:root "$SYS/$i"
+      chmod 644 "$SYS/$i"
+      chmod 755 "$(dirname "$SYS/$i")"
+    done
+    unmount_extras
+    chmod 600 $SYS/build.prop
+  ;;
+esac
